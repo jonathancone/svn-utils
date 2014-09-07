a45de03e60f1271187012e74c267cf6dcfcdc553
unknown <JCone@USM-L-0054.int.cpaglobal.net>
2014-09-06 19:49:46 -0500

    Initial commit.

---
+++ svn-multilog.py
@@ -0,0 +1,17 @@
+from subprocess import call
+import sys
+
+
+projects_file = sys.argv[1]
+svn_path = sys.argv[2] 
+oldest_rev = sys.argv[3]
+
+with open(projects_file) as f:
+    repo_paths = f.readlines()
+
+print [svn_path + line[:-1] for line in repo_paths]
+
+paths = [svn_path + line[:-1] for line in repo_paths]
+
+for path in paths:
+	call(['svn', 'log', path, '-v', '--stop-on-copy', '-r', 'HEAD:' + oldest_rev])
